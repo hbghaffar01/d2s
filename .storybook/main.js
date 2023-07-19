8a302eca982c576659f646e7cc73cc8910abe247
haseeb-ghaffar <hbghaffar01@gmail.com>
2023-07-19 01:58:38 -0700

    storybook updation pending

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,14 @@
-module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {}
   },
-  "features": {
-    "storyStoreV7": true
+  docs: {
+    autodocs: "tag"
   }
-}
+};
+export default config;
+export const framework = "@storybook/vue3";
